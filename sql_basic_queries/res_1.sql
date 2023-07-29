6d48896ac96a2c7a944f548ca7158679d2cdd592
Victor  Maina <122088127+Ds2023@users.noreply.github.com>
2023-07-30 02:06:43 +0300

    Add files via upload

---
+++ sql_basic_queries/res_1.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select FIRST_NAME
+as Worker_Name
+from Worker
+LIMIT 0, 1000
+
+-- Date: 2023-07-30 02:03
+*/
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Monika');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Niharika');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Vishal');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Amitabh');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Vivek');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Vipul');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Satish');
+INSERT INTO `Worker` (`Worker_Name`) VALUES ('Geetika');
